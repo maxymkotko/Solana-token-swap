4ddd9ceee73b090d719cb99a523becb0a4fc2d55
maxymkotko <maxkotko@outlook.com>
2023-10-25 00:13:46 +0530

    chore: call instructions from lib

--- programs/exchange/src/instructions/mod.rs
+++ programs/exchange/src/instructions/mod.rs
@@ -2,13 +2,13 @@ pub mod deposit_all_tokens;
 pub mod deposit_single_token;
 pub mod initialize;
 pub mod swap;
-pub mod withdraw_single_token_acount;
+pub mod withdraw_single_token_out;
 
 pub use deposit_all_tokens::*;
 pub use deposit_single_token::*;
 pub use initialize::*;
 pub use swap::*;
-pub use withdraw_single_token_acount::*;
+pub use withdraw_single_token_out::*;
 
 pub enum TradeDirection {
     TokenAtoB,
--- programs/exchange/src/lib.rs
+++ programs/exchange/src/lib.rs
@@ -19,4 +19,33 @@ pub mod exchange {
     pub fn initialize(ctx: Context<InitializePool>, fees: Fee) -> Result<()> {
         ctx.accounts.initialize(fees)
     }
+
+    pub fn swap(ctx: Context<Swap>, source_amount: u64) -> Result<()> {
+        ctx.accounts.process_swap(source_amount)
+    }
+
+    pub fn deposit_all_tokens_in(
+        ctx: Context<DepositAllTokens>,
+        pool_tokens: u64,
+        max_token_a: u64,
+        max_token_b: u64,
+    ) -> Result<()> {
+        ctx.accounts
+            .deposit_all_tokens_in(pool_tokens, max_token_a, max_token_b)
+    }
+
+    pub fn deposit_single_token(
+        ctx: Context<DepositSingleToken>,
+        source_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts.deposit_single_token_in(source_amount)
+    }
+
+    pub fn withdraw_single_token_out(
+        ctx: Context<WithdrawSingleToken>,
+        source_amount: u64,
+        fees: Fee,
+    ) -> Result<()> {
+        ctx.accounts.withdraw_single_token_out(source_amount, fees)
+    }
 }
